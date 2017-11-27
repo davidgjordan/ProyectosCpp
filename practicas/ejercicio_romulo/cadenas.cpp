15192d69622d283b50fe176ed03cb7e86b6a84cf
davidgjordan <davidgjordancbba@gmail.com>
2017-11-27 17:48:39 -0400

    modificaciones ejercicio romulo jal

--- practicas/ejercicio_romulo/cadenas.cpp
+++ practicas/ejercicio_romulo/cadenas.cpp
@@ -1,42 +1,12 @@
-/* EN ESTA PRIMERA ITERACION 
- QUE COSAS NECESITAMOS PARA UTILIZAR ESTE ASUNTO
- 1 ENTENDER QUE ES LO QUE QUIERO OPTENER LOGRAR EN LA PRIMERA ITERACION
-    ATACAR LO MAS BASICO 
- 2 COMO LOGRAMOS MODELAR EL ASPECTO MAS SUPEFICIAL DE NUESTRO TEMA
- 3 ESCRIBIR HISTORIAS LO MAS GRANULAR POSIBLE
- 4 TENER CLARO Q HAY Q ACER DIVIDIRLO EN VARIAS HISTORIAS
-*/
-
-/* 
-
-
-
-// EJERCICIO
-"TENGO UN TEXTO CAULQUIERA."
-
-LO QUE QUIERO ES TRABAJAR CON COMIDINES WILDCARDS
-
-DARE OTRO TEXTO ASI 
-"???g*"
-
-en este solo hay o ? o *
-
-estas dos cadenas emparejan o no
-
-la reglas es q  ? representa cualquier caracter  solo uno 
-y  * representa cualquier cantidad de caracteres  y esto cauqlueir caracter de lo q sea de 1 a n caracteres
-
-
-recibe dos entradas el texto y el patron(ejemplo"*a*a") si acepater matcher o no  
-    significa q en el final del texto tiene q aver una a
-no puede tener dos * seguidos
- sin usar string principalmente */
 
 #include <iostream>
 
 using namespace std;
 
 bool isValid(const char *texto, const char *patron);
+bool isPatronValid(const char *patron);
+bool hayAsterisco(const char * patron, const char * texto, int countPatron , int countTexto );
+
 class obj
 {
   public:
@@ -44,43 +14,129 @@ class obj
     obj() {}
     obj(int x) : x{x} {}
 };
+
+
 void show(const obj *o);
-void show(obj *o);
+void show1(obj *o);
 
 int main()
 {
 
-    const char *texto = "Primer Texto";
-    const char *patron = "*?i*";
-
-    const obj *t1 = new obj(10);
-    show(t1);
-
-    const *obj t2(5);
+    
+    /*  const obj *t1 = new obj(10);
+    show(t1); //t1 = &t1[0]
+    cout << "**************************" << endl; 
+    
+    const obj t2(5);
     show(&t2);
+    
+    cout << "**************************" << endl; 
+    obj t3;
+    t3.x = 15;
+    show1(&t3); */
+    
+    // &texto[0], patron SON IGUALES
+    
+    const char *texto = "primer texto hola como estas";
+    auto *patron = "*?x?*";
 
-    obj *t3;
-    t3->x = 15;
-    show(t3);
+    if(isValid(&texto[0],patron)){
+         cout<<"El patron es valido"<<endl;
+    }else{
+         cout<<"El patron no es valido"<<endl;
+    }
 
-    // bool is_valid = isValid(texto, patron);
+    // cout<<patron<<"aasd"<<endl;
+
     return 0;
 }
-bool isValid(const char *texto, const char *patron)
-{
-    cout << texto << endl;
-    cout << patron << endl;
+bool isValid(const char *texto, const char *patron){
+    /* 
+     cout << texto << endl;         //primer texto hola como estas
+    cout << *texto << endl;         //p
+    cout << &texto << endl;         //0x7ffe9cb74dc8
+    cout << *(&texto+1) << endl;    //@
+    cout <<"************" << endl;              
+    cout << patron << endl;         //*?x?*
+    cout << &patron << endl;        //0x7ffe9cb74dc0
+    cout << &patron[0] << endl;    //*?x?*
+    cout << *patron << endl;        //*
+    cout << *(&patron) << endl;    //*?x?*
+    cout << *(&patron+1) <<endl;   //primer texto hola como estas  
+    cout << (&patron+1) <<endl;    //0x7ffe9cb74dc8
+    cout << (patron+1) << endl;     //?x?* 
+    cout << *(patron+1)<< endl;     //? 
+    cout <<"************" << endl; */
+    if(isPatronValid(patron) ){
+        //cout<<(int)(*(patron+2))<<" is valid"<<endl;
+        int countPatron=0;
+        int countTexto=0;
+        
+        while(*patron){
+            //lo q importa son las letras y verifico si su n-1 es ? y su n-2 es *
+            //si es letra
+            if((int)(*patron) >= 95 && (int)(*patron) <= 122){
+                cout<<*patron<<"Es letra"<<endl;
+                //recorer la cadena para verificar si existe la letra
+                while(*texto){ 
+                    if(*patron == *texto ){
+
+                        if( (countPatron == countTexto) || countPatron < countTexto  ){
+                            //esta bien
+                            cout<<"entre"<<endl;
+                        }else{
+                            return false;
+                        }
+                        countTexto=0;
+                        countPatron=0;
+                    }else{
+                        countTexto++;
+                        *(texto++);
+                    }
+                }
+
+                // return true;   
+            }
+
+            countPatron++;
+
+            *(patron++);
+        }
+
+        return true;
+
+    }else{
+        return false;
+    }
 
     return true;
 }
 
-void show(const obj *o)
-{
+bool isPatronValid(const char *patron){
+    while(*patron){
+        if(*patron == '*' && *patron == *(patron+1)){
+            return false;
+        }
+        *(patron++);
+    } 
+    return true;        
+}
+
+bool hayAsterisco(const char * patron, const char * texto, const char * countPatron , const char * countTexto ){
+    cout<<patron<<endl;
+    cout<<texto<<endl;
+    cout<<countPatron<<endl;
+    cout<<countTexto<<endl;
+
+    return true;
+}
+
+
+void show(const obj *o){
     cout << o->x << endl;
 }
 
-void show(obj *o)
-{
+void show1(obj *o){
     cout << o->x << endl;
 }
 //  ************************COMPETENCIAS*******************************
