758219575e509a1b813e968d1b2b378ee4ba7771
davidgjordan <davidgjordancbba@gmail.com>
2017-12-16 11:52:37 -0400

    add modificaciones thin3

--- thinker3/source/argumentos.py
+++ thinker3/source/argumentos.py
@@ -1,4 +1,27 @@
 import sys
+import json
 
-for elements in sys.argv:
-    print elements
+print sys.argv[1]
+print sys.argv[1][1]
+
+# for elements in sys.argv:
+#     # print("ini for")
+#     d = json.loads(sys.argv)
+#     # print elements
+#     print d['name']['pi']
+#     # print("fin for")
+
+# print("fin python")
+try:
+    js = json.loads(sys.argv[1])
+
+    # pretty printing of json-formatted string
+    print json.dumps(js, sort_keys=True, indent=4)
+
+    print "JSON parsing example: ", js['name']
+    print "JSON parsing example: ", js['game']
+    print "JSON parsing example: ", js['velocity']
+    print "Complex JSON parsing example: ", js['object']['value']
+
+except (ValueError, KeyError, TypeError):
+    print "JSON format error"
--- thinker3/source/main.cpp
+++ thinker3/source/main.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <fstream>
 #include <Python.h>
+#include <json.hpp>
 
 #define pi 3.141592653589793
 
@@ -10,57 +11,45 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    //     cout<<"hello"<<endl;
-    //     // cout<<js.dump();<<endl;
-    //     //Inicio el interprete Python e imprimo informacion relevante
-    //Py_Initialize();
-    //PyRun_SimpleString("print 'hello world python'");
-    //      //****
-    //     PyObject *FileScript;
-    //     FileScript = PyFile_FromString("script.py","r");
-    //     cout<<"1"<<endl;
-    //     PyRun_SimpleFile(PyFile_AsFile(FileScript),"r");
-    //     cout<<"2"<<endl;
-    //     PyObject *retorno, *modulo, *clase, *metodo, *argumentos, *objeto;
-    //     cout<<"3"<<endl;
-    //     int *resultado;
-    //     //modulo = PyImport_ImportModule("script");
-    //     cout<<"4"<<endl;
-    //     clase = PyObject_GetAttrString(modulo, "Numeros");
-    //     cout<<"5"<<endl;
-    //     argumentos = Py_BuildValue("ii",5,11);
-    //     cout<<"6"<<endl;
-    //     objeto = PyEval_CallObject(clase, argumentos);
-    //     cout<<"7"<<endl;
-    //     metodo = PyObject_GetAttrString(objeto, "suma");
-    //     cout<<"8"<<endl;
-    //     argumentos = Py_BuildValue("()");
-    //     cout<<"9"<<endl;
-    //     retorno = PyEval_CallObject(metodo,argumentos);
-    //     cout<<"10"<<endl;
-    //     PyArg_Parse(retorno, "i", &resultado);
-    //     cout<<"11"<<endl;
-    //     cout<<"El resultado es: "<<(int)(*resultado)<<endl;
-    //     cout<<"12"<<endl;
+    std::cout << "Hello world" << '\n';
+    json js = {
+        {"\"pi\"", 3.141},
+        {"\"happy\"", true},
+        {"\"name\"", "\"Niels\""},
+        {"\"game\"", "\"Pacman\""},
+        {"\"velocity\"", 3},
+        {"\"answer\"", {{"\"everything\"", 42}}},
+        {"\"list\"", {1, 0, 2}},
+        {"\"object\"", {{"\"currency\"", "\"USD\""}, {"\"value\"", 42.99}}}};
 
-    // // //***********
-    //     Py_Finalize();
-    // 	char terminar;
-    // 	cin>>terminar;
+    IPythonScript *python = new PythonByHand("path enviado");
+    python->addParameter(js);
 
-    //
-
-    // Py_SetProgramName(argv[0]); /* optional but recommended */
+    python->run();
+    //************************************************
+    //Inicio el interprete Python e imprimo informacion relevante
     // Py_Initialize();
-    // PyRun_SimpleString("from time import time,ctime\n"
-    //                    "print 'Today is',ctime(time())\n");
-    // //FILE * pFile;
-    // PyObject *FileScript;
-    // FileScript = PyFile_FromString("script.py", "r");
-    // PyRun_SimpleFile(PyFile_AsFile(FileScript), "r");
+
+    // PyRun_SimpleString("print hello world python");
+
     // Py_Finalize();
 
-    system("python argumentos.py uno dos tres cuatro");
+    // char terminar;
+    char *fin;
+    const char *src1 = "python ../source/argumentos.py ";
+    string dum = js.dump();
+    const char *src2 = dum.c_str();
+    auto len = strlen(src1) + strlen(src2);
+    fin = new char[len + 1];
+    strcpy(fin, src1);
+    strcat(fin, src2);
+    cout << "********************************************************" << endl;
+    cout << js.dump() << endl;
+    cout << fin << endl;
+    cout << "********************************************************" << endl;
+
+    //system("python ../source/argumentos.py uno dos tres");
+    system(fin);
 
     return 1;
 }
