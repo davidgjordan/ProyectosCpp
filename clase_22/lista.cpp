9be926b5f6514fda9bbf4db283f2224e9a09b509
david.granado <david.granado@fundacion-jala.org>
2017-12-21 11:07:15 -0400

    classe macros

---
+++ clase_22/lista.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <cstdio>
+#include <stdexcept>
+//listas genericas con macros
+using namespace std;
+# define  DEF_LL(T,LL)\
+struct LL##Node{\
+    T elem;\
+    LL##Node * next;\
+};\
+struct LL{\
+    LL##Node * first = nullptr;\
+    LL##Node * last = nullptr;\
+};\
+void add(LL & x, const T & y){\
+    auto nn = new LL##Node{y, nullptr};\
+    if(x.first==nullptr){\
+        x.first = x.last=nn;\
+        return;\
+    }\
+    x.last->next = nn;\
+    x.last = nn;\
+}
+
+DEF_LL(int , IntelicenceList)
+DEF_LL(string , StringLinkedList)
+
+//__file__  devuelve el numero
+//__line__  devuelve el numero de linea 
+int main(){
+    IntelicenceList a;
+    add(a,15);
+    add(a,15);
+    add(a,15);
+    add(a,15);
+    auto aux =a.first;
+    while(aux){
+        cout<<aux->elem<<endl;
+        aux =aux->next;
+    }
+
+    StringLinkedList b;
+        add(b,"15gfj");
+        add(b,"15gfj");
+        add(b,"15gfj");
+        add(b,"15gfj");
+        auto auxs =b.first;
+        while(auxs){
+            cout<<auxs->elem<<endl;
+            auxs =auxs->next;
+        }
+
+    return 0;
+}
---
+++ clase_22/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <cstdio>
+#include <stdexcept>
+# define GET_ABS(n)  (n<0)?-n:n
+# define  LESS_THAT =
+# define  FLOAT int
+# define  END;
+# undef true  // desdeinimos true le quitamos el valor
+#define true 0
+#define FOR_EACH(name , arr,n)\
+        for(int __x=0;__x<n;__x++){\
+            auto name =  arr[__x];
+#define END_FOR_EACH }
+            
+            // GET_ABS(arr[__x])\
+
+using namespace std;
+//macros caractesursctica del preprocesador de c mas lineas de codigo se puede?
+int getAbs(int n){
+    return n<0 ? -n:n;
+}
+
+int  main(){
+
+        // int x = getAbs(-15);
+        // int x = GET_ABS(-15);
+        // cout<<x<<endl;
+        // FLOAT Y LESS_THAT 5 END
+
+        // bool z = true;
+        // cout<<z<<endl;
+
+        int x []={1,-2,3};
+        FOR_EACH(i,x,3)
+        cout<<i<<endl;
+
+        END_FOR_EACH
+        
+
+    return 0;
+}
