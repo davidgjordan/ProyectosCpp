9f7476103844179cc7a25d33a7d30786baad3044
david.granado <david.granado@fundacion-jala.org>
2018-01-18 17:39:11 -0400

    add smart pointers

--- clase_30_smart_pointers/main.cpp
+++ clase_30_smart_pointers/main.cpp
@@ -5,7 +5,6 @@
 //IMPLEMENTA ALGUN MECANISMO DE MANEJO DE MEMORIA DEPENDIENDO 
 //DEL SMART POINTER ELEGIMOS Q MECANISMO UTILIZAR
 
-
 //evitar  lo mas posible new o usar smart pointer
 #include <iostream>
 #include <string>
@@ -13,7 +12,7 @@
 #include <cstdlib>
 #include <cmath>
 #include <cstdlib>
-#include <algorithm>
+#include <memory> //contiene punteros inteligentes hay tres
 
 using namespace std;
 
@@ -44,6 +43,42 @@ struct Ptr{
     }
 };
 
+struct X{
+    int  n;
+    void print(){
+        std::cout << n << '\n';
+    }
+    X(int n):n{n}{}
+
+    ~X(){
+        std::cout << "bye X" << '\n';
+        
+    }
+};
+
+struct Point{
+    int x,y;
+    Point(int x, int y):x{x},y{y}{
+        std::cout << "pointer" << '\n';
+        //throw 1;
+    }
+    ~Point(){
+        std::cout << "bye pointer" << '\n';
+    }
+};
+
+struct Line{
+    unique_ptr<Point>p1;//con esto nos olvidamos acer deletes de los punteros
+    unique_ptr<Point>p2;//con esto nos olvidamos acer deletes de los punteros
+    Line(Point *p1, Point *p2):p1{p1},p2{p2}{
+        throw 8;
+    }
+    ~Line(){
+        //delete p1;
+        //delete p2;
+    }
+};
+
 int main(){
 
 
@@ -51,11 +86,37 @@ int main(){
         //A * pa = new A{new int{8}};//yo tengo q destruir a mano mi objeto
         //mejor smart points
 
-    Ptr<A> p{new A{new int{10}}};//prt es uhn puntero a A declarado en el heap 
+    /* Ptr<A> p{new A{new int{10}}};//prt es uhn puntero a A declarado en el heap 
                                 //el PTR se encarga de acer delete
 
     p->show();
     (*p).show();//accedo a su valor y se vuelve un objeto
+
+ */
+    //TTRES PUTNEROS INTELIGENTE //SU POLITICA ES PARECIDA A LA Q IMPLEMENTAMOS
+    unique_ptr<X> p{new X{6}};//abia antes auto_ptr esta deprecado
+    p->print();
+    //es lo mismo q el de arrva solo cambia la sintaxis
+    auto q = make_unique<X>(20);
+    q->print();
+
+    //esta borrado q le tenemos q mover la implementacion
+    {
+        //le estoy robando la implementacion q
+        auto r = move(q);
+    }
+    std::cout << "mort" << '\n';
+
+    std::cout << "****************" << '\n';
+    try{
+
+        Line line{new Point(2,4), new Point(5, 8)};
     
+    }
+        catch(...){
+            std::cout << "error" << '\n';
+        }
+    
+
     return 0;
 }
