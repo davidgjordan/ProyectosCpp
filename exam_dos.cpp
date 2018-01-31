b965a04ae2a36fda5f6ba9061872588f2a316c55
david.granado <david.granado@fundacion-jala.org>
2018-01-31 16:09:35 -0400

    exam_dos

--- exam_dos.cpp
+++ exam_dos.cpp
@@ -12,37 +12,36 @@
 using namespace std;
 
 //*****************INI PILA*************************************
-struct NodePila{//para guardar los numeros 
-    double data;
-    NodePila * siguiente;
-    NodePila(double sdata, NodePila *snode =nullptr):data{sdata}, siguiente{snode}{
-    }
-    public:
-    ~NodePila(){
-    }
-    friend class Pila;
-};
-
-class Pila{
-    public:
-    NodePila * ultimo = nullptr;
-    public:
-    void push(double data){
-        auto n = new NodePila(data,ultimo );
-        ultimo = n;
-        cout<<"push"<<endl;
-    }
-
-    double pop(){
-        if(!ultimo)return 0;
-        auto aux = ultimo;
-        double data = aux->data;
-        ultimo = ultimo->siguiente;
-        delete aux;
-        cout<<"pop"<<endl;
-        
-        return data;
-    }
+# define  PILA(T, LL)\
+struct LL##NodePila{\
+    T data;\
+    LL##NodePila * siguiente;\
+    LL##NodePila(T sdata, LL##NodePila *snode =nullptr):data{sdata}, siguiente{snode}{\
+    }\
+    public:\
+    ~LL##NodePila(){\
+    }\
+    friend class Pila;\
+};\
+class LL{\
+    public:\
+    LL(){}\
+     LL##NodePila * ultimo = nullptr;\
+    public:\
+    void push(T data){\
+        auto n = new LL##NodePila(data,ultimo );\
+        ultimo = n;\
+        cout<<"push"<<endl;\
+    }\
+    T pop(){\
+        if(!ultimo)return 0;\
+        auto aux = ultimo;\
+        T data = aux->data;\
+        ultimo = ultimo->siguiente;\
+        delete aux;\
+        cout<<"pop"<<endl;\
+        return data;\
+    }\
 };
 //*****************FIN PILA*************************************
 
@@ -53,49 +52,51 @@ enum class ECalculatorType{
 };
 class IOperator{
 };
-class IOperatorBinario : public virtual IOperator{
+class OperatorBinario : public virtual IOperator{
   public:  virtual double eval(double &a, double &b)=0;
+  ~OperatorBinario(){}
 };
-class IOperatorUnario : public virtual IOperator{
-  public:  virtual double eval(double &a)=0;    
+class OperatorUnario : public virtual IOperator{
+  public:  virtual double eval(double &a)=0;
+  ~OperatorUnario(){} 
 };
-class OperatorAdd :public virtual IOperatorBinario{
+class OperatorAdd :public virtual OperatorBinario{
      public:
      double eval(double &a, double &b){
          return a+b;
      }    
 };
-class OperatorMul :public virtual IOperatorBinario{
+class OperatorMul :public virtual OperatorBinario{
      public:
      double eval(double &a, double &b){
          return a*b;
      }    
 };
-class OperatorResta :public virtual IOperatorBinario{
+class OperatorResta :public virtual OperatorBinario{
      public:
      double eval(double &a, double &b){
          return a-b;
      }    
 };
-class OperatorDiv :public virtual IOperatorBinario{
+class OperatorDiv :public virtual OperatorBinario{
      public:
      double eval(double &a, double &b){
          return a/b;
      }    
 };
-class OperatorMax:public virtual IOperatorBinario{
+class OperatorMax:public virtual OperatorBinario{
     public:
      double eval(double &a, double &b){
          return a>b?a:b;
      }    
 };
-class OperatorInc :public virtual IOperatorUnario{
+class OperatorInc :public virtual OperatorUnario{
      public:
      double eval(double &a){
          return a++;
      }    
 };
-class OperatorSqrt :public virtual IOperatorUnario{
+class OperatorSqrt :public virtual OperatorUnario{
      public:
      double eval(double &a){
          return sqrt(a);
@@ -129,7 +130,7 @@ struct ListaOperadoresExistentes{
  		}
          
     }
-     void add_front(string nkey, IOperator * operatorE){
+     void push(string nkey, IOperator * operatorE){
         auto n = new NodeLis{nkey,operatorE, nullptr, nullptr};
         if(first == nullptr){
             first = last = n;
@@ -139,7 +140,6 @@ struct ListaOperadoresExistentes{
             first = n;
         }
     }
-
     bool existe(string& nkey){
         auto n = first;
         if(n==nullptr){
@@ -151,6 +151,21 @@ struct ListaOperadoresExistentes{
             }
             n = n->siguiente;
         }
+        return false;
+    }
+
+    IOperator * getOp(string& nkey ){
+        auto n = first;
+        if(n==nullptr){
+            return nullptr;
+        }
+        while(n){
+            if(n->key.compare(nkey) == 0){
+                return n->operatorE;
+            }
+            n = n->siguiente;
+        }
+        return nullptr;
     }
 };
 //
@@ -158,55 +173,71 @@ struct ListaOperadoresExistentes{
 class ICalculator{
     public:
     virtual double eval(const string& exprexion)=0;
-    virtual void add_operator(string & key, IOperator)=0;
+    virtual void add_operator(string & key, IOperator*)=0;
 };
 
+PILA(double, PilaNumbers)
+PILA(string, PilaOperator)
 class CalculatorRPN : public virtual ICalculator{
-Pila pila ;
-public:
-    //double num2 = std::stod(auxChar2);
-    double eval(const string& exprexion){
-        string exp = (string)exprexion;
-        string aux;
-        stringstream input(exp);
-        while(getline(input,aux,' ')){
-            double d =  stod(aux);
-            if(d){
-                cout<<"entre"<<d<<endl;
-                pila.push(d);
-            }
-            aux ="";
+    PilaNumbers  pilanumbers;
+    PilaOperator pilaoperator;
+    ListaOperadoresExistentes lsoper;
+    public:
+        CalculatorRPN(){
+            lsoper.push("+", new OperatorAdd());
+            lsoper.push("-", new OperatorResta());
+            lsoper.push("*", new OperatorMul());
+            lsoper.push("/", new OperatorDiv());
         }
+        //double num2 = std::stod(auxChar2);
+        double eval(const string& exprexion){
+             string exp = (string)exprexion;
+         string aux;
+         stringstream input(exp);
+         while(getline(input,aux,' ')){
+             try{
+                 if(stod(aux)){
+                     double d =  stod(aux);
+                     cout<<"pilanumbers: "<<d<<endl;
+                     //double d = stod(aux);
+                     pilanumbers.push(d);
+                }
+             }catch(...){
+                 pilaoperator.push(aux);                
+                 std::cout << "pilaoperator "<<aux << '\n'; 
+             }
+             //aux ="";
+         }
+        double res;
+        while(pilaoperator.ultimo){
+            string key = pilaoperator.pop();
+            if(lsoper.existe(key)){
+                IOperator * oper = lsoper.getOp(key);
+                OperatorBinario * operBinary = dynamic_cast<OperatorBinario *>(*(&oper));
 
-    
-        // if(getline(input_stringstream,parsed,' '))
-        // {
-        //     // do some processing.
-        // }
-            cout<<pila.ultimo->data<<endl;
-        showp();
-
+            }else{
+                cout<< "[Error: operator "<<"\'"<<key<<"\'"<<" unknown]"<<endl;
+                res = -1;    
+            }
+        }
+        return res;
     }  
 
-    void showp(){
-       // while(pila.ultimo){
-        //}
-    }
-
-    void add_operator(string & key, IOperator){
-
+    void add_operator(string & key, IOperator *oper){
+        lsoper.push(key, oper);
     }
 };
 
 class CalculatorFactory{
 public:
-   
+
     ICalculator *  create_calculator(const ECalculatorType type){
         if(type == ECalculatorType::RPN ){
             return new CalculatorRPN();
         }
         return nullptr;
     }
+
 };
 
 void show(double res){
@@ -219,6 +250,18 @@ int main()
         CalculatorFactory cf;
         ICalculator* calc = cf.create_calculator(ECalculatorType::RPN);
         show(calc->eval("2 3 +")); // 5
+
+        /* Pila p;
+        p.push(5);
+        p.push(53);
+        p.push(51);
+        p.push(4);
+
+
+        while(p.ultimo){
+            cout<<p.pop()<<endl;
+       } */
+
         //calc->showp();
         
 /* int main()
